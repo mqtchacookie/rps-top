6be1ed3b36e99b2b80a3b988549c00eee05447be
matchacookie <tongkhaiminh@gmail.com>
2022-05-31 17:11:42 +0700

    finish project

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
-let x = ['rock', 'paper', 'scissors']
-
 let computerPlay = (list) => {
-	console.log(`${x[Math.floor(Math.random() * list.length)]}`)
+	let move = list[Math.floor(Math.random() * list.length)]
+	// console.log(`${move}`)
+	return move
+}
+let round = (pSelect, cSelect, roundNumber) => {
+	let lowerCase = pSelect.toLowerCase()
+
+	if (lowerCase === cSelect) {
+		console.log('tie')
+		return 'tie'
+	}
+	if (lowerCase === 'rock' && cSelect === 'scissors') {
+		console.log('win')
+		return 'win'
+	}
+	if (lowerCase === 'paper' && cSelect === 'rock') {
+		console.log('win')
+		return 'win'
+	}
+	if (lowerCase === 'scissors' && cSelect === 'paper') {
+		console.log('win')
+		return 'win'
+	} else {
+		console.log('lose')
+		return 'lose'
+	}
 }
 
-computerPlay(x)
+let playGame = (selectP, selectC, score) => {
+	if (round(selectP, selectC) === 'win') return (score = score + 1)
+	return score
+}
+let score = 0
+let options = ['rock', 'paper', 'scissors']
+
+for (let i = 0; i < 5; i++) {
+	let choice = prompt('pick: rock paper scissors')
+	score = playGame(choice, computerPlay(options), score)
+	console.log(score)
+}
