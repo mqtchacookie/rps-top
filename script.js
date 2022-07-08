19f3c87024ea036a02b097f0c6f0a5cde3a60cea
matchacookie <tongkhaiminh@gmail.com>
2022-07-08 12:22:43 +0700

    Make buttons fully functional

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+// COMPUTER RNG
 let computerPlay = (list) => {
 	let move = list[Math.floor(Math.random() * list.length)];
 	// console.log(`${move}`)
 	return move;
 };
+
+// ROUND LOGIC
 let round = (pSelect, cSelect) => {
 	let lowerCase = pSelect.toLowerCase();
 
@@ -26,27 +29,32 @@ let round = (pSelect, cSelect) => {
 		return 'lose';
 	}
 };
+
 // Check if round was a win
 let playGame = (selectP, selectC, score) => {
 	if (round(selectP, selectC) === 'win') return (score = score + 1);
 	return score;
 };
+
 // Var declaration
 let score = 0;
 let options = ['rock', 'paper', 'scissors'];
+let player = '';
 
 // Button System
 let rock = document.querySelector('.rock');
 rock.addEventListener('click', () => {
-	console.log('rock');
+	playGame('rock', computerPlay(options), score);
 });
 
 let paper = document.querySelector('.paper');
 paper.addEventListener('click', () => {
-	console.log('paper');
+	playGame('paper', computerPlay(options), score);
 });
 
 let scissor = document.querySelector('.scissor');
 scissor.addEventListener('click', () => {
-	console.log('scissor');
+	playGame('scissor', computerPlay(options), score);
 });
+
+//
